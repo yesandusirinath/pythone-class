62b626d304b694758bce562f83cc20c5864270cf
Yesandu <ysirinath2011@gmail.com>
2025-11-17 18:17:46 +0000

    basic calulator

---
+++ class 2/arithmetic operators.py
@@ -0,0 +1,14 @@
+#print
+print ("i am akash",12,90.5)
+print (1,2,3,4,5)
+print (345,78.8,"yesandu")
+# \n: placeholder that tells the computer to change the line
+print ("yesandu\n"*3)
+# arithmetic operators: +,-,*,/,%(modulus),**(power),//(floor div)
+print ("addition of 10 and 23 is: ",10+23)
+print ("subtraction is: ",12-4)
+print ("multiplication is: ",10*20)
+print ("div is: ",25/3)
+print ("floor div is: ",25//3)
+print ("remainder is: ",24 % 5)# %:modulus sybol: for remiander
+print("base 2 ,power 3 is:",2**3)
---
+++ class 2/basic calulator.py
@@ -0,0 +1,12 @@
+#basic calulator
+# variable
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+print("x",x,"y",y)
+print("add",x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x//y)
+print(x**y)
