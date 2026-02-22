a70d3bd2de642e27b9240ee100e914c93f3dd5f1
yesandu <ysirinath2011@gmail.com>
2026-02-22 19:24:43 +0000

    Sum of Squares of Digits

--- class 16/Sum of Squares of Digits.py
+++ class 16/Sum of Squares of Digits.py
@@ -3,9 +3,6 @@ f= n
 sum = 0
 while n>0 :
     dig = n%10
-    print (f"dig is {dig}")
     sum += dig**2
-    print (f"the sum is {sum}")
     n = n//10
-    print (f"n is {n}")
 print (f" Sum of Squares of {f} is {sum}")
